d32ca9682fb6f0d26c86c2767b9a1e8fdaf534c7
Elsabe Ros <ElsabeR@discovery.co.za>
2018-12-03 11:02:02 +0200

    Day 2 part 1.

---
+++ src/Day2_1.kts
@@ -0,0 +1,14 @@
+import java.io.File
+import java.io.InputStream
+
+val file: File = File("../input/day2.txt")
+
+val twoLetters = file.inputStream().bufferedReader().lines().filter {
+    it.groupingBy { it }.eachCount().containsValue(2)
+}.count();
+
+val threeLetters = file.inputStream().bufferedReader().lines().filter {
+    it.groupingBy { it }.eachCount().containsValue(3)
+}.count();
+
+println(twoLetters * threeLetters)
