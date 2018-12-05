ef5ccd4e1ec98f08be682ff085d8053660842459
Elsabe Ros <ElsabeR@discovery.co.za>
2018-12-05 08:29:12 +0200

    Day 5 part 1.

---
+++ src/Day5_1.kts
@@ -0,0 +1,22 @@
+import java.io.File
+
+val file = File("../input/day5.txt")
+val input = file.readLines().first()
+
+val result = input.map { c -> c.toString() }.reduce { acc, c ->
+    if (acc.isEmpty()) {
+        c
+    } else if (complements(acc.last(), c.toCharArray().first())) {
+        acc.substring(0, acc.length - 1)
+    } else {
+        "$acc$c"
+    }
+}
+
+println(result.length)
+
+fun complements(one: Char, two: Char): Boolean {
+    return one.toLowerCase() == two.toLowerCase() &&
+            ((one.isUpperCase() && two.isLowerCase())
+                    || (one.isLowerCase() && two.isUpperCase()))
+}
